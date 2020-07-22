9db2753bd3706ff276bb93a8910542494eb6bf2d
YoseptF <yosept.flores@gmail.com>
2020-07-22 08:09:49 -0500

    minor changes to how the search api responds

--- app/controllers/api/V1/search_controller.rb
+++ app/controllers/api/V1/search_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::SearchController < ApplicationController
       render json: { error: "this page doesn't exist" }, status: :bad_request
     else
       @games = gameslices[params[:page].to_i - 1 || 0]
-      @liked_games = current_user.liked_games
+      @liked_games = current_user.nil? ? nil : current_user.liked_games
       render json: {
         items: format(@games),
         itemsLike: format(@liked_games),
         pages: gameslices.length,
-        likes: current_user ? current_user.liked_games.map(&:id) : []
+        likes: @liked_games.nil? ? [] : @liked_games.map(&:id)
       }, status: :ok
     end
   end
@@ -18,6 +18,8 @@ class Api::V1::SearchController < ApplicationController
   private
 
   def format(game)
+    return [] if game.nil?
+
     game.map do |g|
       {
         image: g.cover.attached? ? url_for(g.cover) : 'https://via.placeholder.com/100',
